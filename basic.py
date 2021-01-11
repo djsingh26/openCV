7c47fc27bbf21171d64780f9bfec3cbd5c6e03ea
djsingh26 <dilraj3004@gmail.com>
2021-01-10 19:19:02 -0500

    transformations added

--- basic.py
+++ basic.py
@@ -1,11 +1,36 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')
+img = cv.imread('Photos/park.jpg')
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
 
 # Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
+
+# Blur
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge Cascade
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=1)
+cv.imshow('Dialated', dilated)
+
+#Eroding
+eroded = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('Eroded', eroded)
+
+# Resize and Crop
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
+
 
 cv.waitKey(0)
